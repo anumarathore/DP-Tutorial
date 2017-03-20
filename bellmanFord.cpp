3ebb900f7925acaf13336084307d9a71546361d0
Anuma Rathore <anchanusis@gmail.com>
2017-03-20 09:34:58 +0530

    Update bellmanFord.cpp

--- bellmanFord.cpp
+++ bellmanFord.cpp
@@ -1,119 +1,90 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
- 
-struct Edge
-{
-        int src, dest, weight;
-};
- 
-struct Graph
-{
-        // V-> Number of vertices, E-> Number of edges
-        int V, E;
- 
-        // graph as an array of edges.
-        struct Edge* edge;
-};
- 
-struct Graph* createGraph(int V, int E)
-{
-    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
-    graph->V = V;
-    graph->E = E;
- 
-    graph->edge = (struct Edge*) malloc(graph->E * sizeof(struct Edge));
- 
-    return graph;
-}
- 
-// printb solution
-void printArr(int dist[], int n)
-{
-    printf("Vertex Distance from Source\n");
-    for (int i = 0; i < n; ++i)
-        printf("%d \t\t %d\n", i, dist[i]);
-}
- 
-void BellmanFord(struct Graph* graph, int src)
-{
-    int V = graph->V;
-    int E = graph->E;
-    int dist[V];
- 
-    //Initialize distances from src to all other vertices as INFINITE
-    for (int i = 0; i < V; i++)
-        dist[i] = INT_MAX;
-    dist[src] = 0;
- 
-    for (int i = 1; i <= V - 1; i++)
-    {
-        for (int j = 0; j < E; j++)
-        {
-            int u = graph->edge[j].src;
-            int v = graph->edge[j].dest;
-            int weight = graph->edge[j].weight;
-            if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
-                dist[v] = dist[u] + weight;
-        }
-    }
- 
-    // negative weight cycles check. 
-    for (int i = 0; i < E; i++)
-    {
-        int u = graph->edge[i].src;
-        int v = graph->edge[i].dest;
-        int weight = graph->edge[i].weight;
-        if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
-            printf("Graph contains negative weight cycle");
-    }
- 
-    printArr(dist, V);
- 
-    return;
-}
- 
-int main()
-{
-    
-    int V = 5; 
-    int E = 8; 
-    struct Graph* graph = createGraph(V, E);
- 
-    graph->edge[0].src = 0;
-    graph->edge[0].dest = 1;
-    graph->edge[0].weight = -1;
- 
-    graph->edge[1].src = 0;
-    graph->edge[1].dest = 2;
-    graph->edge[1].weight = 4;
+#include <cstdio>
+#include <vector>
 
-    graph->edge[2].src = 1;
-    graph->edge[2].dest = 2;
-    graph->edge[2].weight = 3;
+#define INF (1<<31 - 1)
+#define NIL -1
+
+using namespace std;
+
+class bford {
+  
+public:
+  
+ class edge {
+  public:
+ int u, v, w;
+  };
  
-    graph->edge[3].src = 1;
-    graph->edge[3].dest = 3;
-    graph->edge[3].weight = 2;
+  int nVertex;
+  int *d, *p;
+  vector<edge> elist;
+  
+  bford( ) {
+ d = new int[0];
+ p = new int[0];
+ elist.clear();
+  }
+  
+  bool function( int s, int nVertex ) {
  
-    graph->edge[4].src = 1;
-    graph->edge[4].dest = 4;
-    graph->edge[4].weight = 2;
+ d = new int[(const int)nVertex+1];
+ p = new int[(const int)nVertex+1];
  
-    graph->edge[5].src = 3;
-    graph->edge[5].dest = 2;
-    graph->edge[5].weight = 5;
+ int i, j, w, u, v;
+ // init_single_source( s ) {
+ for (i=0 ; i<=nVertex ; i++) {
+  d[i] = INF;
+  p[i] = NIL;
+ }
+ d[s] = 0;
+ // }
  
-    graph->edge[6].src = 3;
-    graph->edge[6].dest = 1;
-    graph->edge[6].weight = 1;
+ for (i=0 ; i<nVertex-1 ; i++) {
+  for ( j=0 ; j<elist.size() ; j++) {
+   // relax( elist[j].u, elist[j].v, elist[j].w ) {
+   u = elist[j].u;
+   v = elist[j].v;
+   w = elist[j].w;
+   
+   if (d[v] > d[u]+w) {
+    d[v] = d[u]+w;
+    p[v] = u;
+   }
+   // }
+  }
+ }
  
-    graph->edge[7].src = 4;
-    graph->edge[7].dest = 3;
-    graph->edge[7].weight = -3;
- 
-    BellmanFord(graph, 0);
- 
-    return 0;
+ for (i=0 ; i<elist.size() ; i++) {
+  if ( d[ elist[i].v ] > d[ elist[i].u ] + elist[i].w ) {
+   return false;
+  }
+ }
+ return true;
+  }
+
+};
+
+int main( void ) {
+  
+  int i, u, v, w, nVertex, nEdge;
+  
+  bford grp;
+  scanf("%d",&nVertex);
+  
+  scanf("%d",&nEdge);
+  
+  for (i=0 ; i<nEdge ; i++) {
+ scanf("%d %d %d",&u,&v,&w);
+ grp.elist.push_back(bford::edge{u,v,w});
+  }
+  
+  printf("%s\n",(grp.function( 1, nVertex )?"True":"False"));
+  
+  for (i=1 ; i<=nVertex ; i++) {
+ printf("%d - %d\n",i, grp.d[i]);
+  }
+  
+  
+  return 0;
+  
 }
